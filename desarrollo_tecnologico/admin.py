d90e4df63c790b8942d421870b4194a8de6b3b21
Nizhi <mathereall@gmail.com>
2016-12-15 12:13:15 -0600

    15 12 2016

--- desarrollo_tecnologico/admin.py
+++ desarrollo_tecnologico/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import TipoDesarrollo, Patente, Licencia, Libro, Editorial, StatusCapituloLibro, CapituloLibro, DesarrolloTecnologico
+from . models import TipoDesarrollo, Proyecto, Licencia, Editorial, StatusPublicacion, Libro, Revista, CapituloLibro, PrologoLibro, ResenaLibro, DesarrolloTecnologico
 
 admin.site.register(TipoDesarrollo)
-admin.site.register(Patente)
+admin.site.register(Proyecto)
 admin.site.register(Licencia)
+admin.site.register(Editorial)
+admin.site.register(StatusPublicacion)
 admin.site.register(Libro)
-#admin.site.register(Editorial)
-admin.site.register(StatusCapituloLibro)
+admin.site.register(Revista)
 admin.site.register(CapituloLibro)
+admin.site.register(PrologoLibro)
+admin.site.register(ResenaLibro)
 admin.site.register(DesarrolloTecnologico)
--- desarrollo_tecnologico/models.py
+++ desarrollo_tecnologico/models.py
@@ -8,34 +8,34 @@ from nucleo.models import Tag, Pais, Estado, Ciudad, Ubicacion, Institucion, Dep
 
 class TipoDesarrollo(models.Model):
     tipo_desarrollo = models.CharField(max_length=255, unique=True)
+    descripcion = models.TextField()
     slug = AutoSlugField(populate_from='tipo_desarrollo', unique=True)
 
     def __str__(self):
         return self.tipo_desarrollo
+    class Meta:
+        ordering = ['tipo_desarrollo']
+        verbose_name = 'Tipo de desarrollo'
+        verbose_name_plural = 'Tipos de desarrollo'
 
-
-class Patente(models.Model):
-    patente = models.CharField(max_length=255, unique=True)
-    slug = AutoSlugField(populate_from='patente', unique=True)
+class Proyecto(models.Model):
+    proyecto = models.CharField(max_length=255, unique=True)
+    descripcion = models.TextField()
+    slug = AutoSlugField(populate_from='proyecto', unique=True)
 
     def __str__(self):
-        return self.patente
-
+        return self.proyecto
+    class Meta:
+        ordering = ['proyecto']
 
 class Licencia(models.Model):
     licencia = models.CharField(max_length=255, unique=True)
+    descripcion = models.TextField()
+    url = models.URLField()
     slug = AutoSlugField(populate_from='licencia', unique=True)
 
     def __str__(self):
         return self.licencia
-
-
-class Libro(models.Model):
-    libro = models.CharField(max_length=255, unique=True)
-    slug = AutoSlugField(populate_from='libro', unique=True)
-
-    def __str__(self):
-        return self.libro
 
 
 class Editorial(models.Model):
@@ -44,45 +44,118 @@ class Editorial(models.Model):
 
     def __str__(self):
         return self.editorial
+    class Meta:
+        verbose_name_plural = 'Editoriales'
 
 
-class StatusCapituloLibro(models.Model):
+class StatusPublicacion(models.Model):
     status = models.CharField(max_length=255, unique=True)
     slug = AutoSlugField(populate_from='status', unique=True)
 
     def __str__(self):
         return self.status
+    class Meta:
+        verbose_name = 'Status de publicacion'
+        verbose_name_plural = 'Status de publicaciones'
+
+
+class Libro(models.Model):
+    libro = models.CharField(max_length=255, unique=True)
+    autores = models.ManyToManyField(User, related_name='libro_autores')
+    editores = models.ManyToManyField(User, related_name='libro_editores')
+    editorial = models.ForeignKey(Editorial)
+    pais = models.ForeignKey(Pais)
+    fecha = models.DateField(auto_now=False)
+    url = models.URLField(blank=True)
+    isbn = models.SlugField(max_length=17)
+    tags = models.ManyToManyField(Tag, related_name='libro_tags')
+    status = models.ForeignKey(StatusPublicacion)
+    slug = AutoSlugField(populate_from='libro', unique=True)
+
+    def __str__(self):
+        return "{} : {} : {} : {}".format(self.libro, self.editorial, self.pais, self.isbn)
+    class Meta:
+        ordering = ['libro']
+        get_latest_by = ['fecha', 'libro', 'editorial']
+
+
+class Revista(models.Model):
+    revista = models.CharField(max_length=255, unique=True)
+    numero = models.CharField(max_length=50, unique=True)
+    editorial = models.ForeignKey(Editorial)
+    pais = models.ForeignKey(Pais)
+    fecha = models.DateField(auto_now=False)
+    url = models.URLField(blank=True)
+    tags = models.ManyToManyField(Tag, related_name='revista_tags')
+    status = models.ForeignKey(StatusPublicacion)
+    slug = AutoSlugField(populate_from='libro', unique=True)
+
+    def __str__(self):
+        return "{} : {} : {}".format(self.revista, self.editorial, self.pais)
+    class Meta:
+        ordering = ['revista']
+        get_latest_by = ['fecha', 'revista', 'editorial']
 
 
 class CapituloLibro(models.Model):
     capitulo = models.CharField(max_length=255)
     libro = models.ForeignKey(Libro)
     autores = models.ManyToManyField(User, related_name='capitulo_libro_autores')
-    editorial = models.ForeignKey(Editorial)
-    pais = models.ForeignKey(Pais)
+    agradecimientos = models.ManyToManyField(User, related_name='capitulo_libro_agradecimientos')
     fecha = models.DateField(auto_now=False)
     pagina_inicio = models.PositiveIntegerField()
-    paginas_fin = models.PositiveIntegerField()
-    agradecimientos = models.ManyToManyField(User, related_name='capitulo_libro_agradecimientos')
-    tags = models.ManyToManyField(Tag, related_name='capitulo_libro_tags')
-    isbn = models.SlugField(max_length=17)
-    status = models.ForeignKey(StatusCapituloLibro) # este campo muy seguramente debería cambiarse a un choices o a una clase
-    url = models.URLField(blank=True)
+    pagina_fin = models.PositiveIntegerField()
 
     def __str__(self):
-        return "{} : {} : {} : {}".format(self.libro, self.capitulo, self.isbn, self.pais)
+        return "{} : {}".format(self.capitulo, self.libro, )
     class Meta:
         ordering = ['libro', 'capitulo']
         get_latest_by = ['fecha', 'libro', 'capitulo']
-        verbose_name_plural = 'Capítulos de Libros'
+        verbose_name = 'Capítulo de libro'
+        verbose_name_plural = 'Capítulos de libros'
+
+
+class PrologoLibro(models.Model):
+    libro = models.ForeignKey(Libro)
+    autores = models.ManyToManyField(User, related_name='prologo_libro_autores')
+    agradecimientos = models.ManyToManyField(User, related_name='prologo_libro_agradecimientos')
+    fecha = models.DateField(auto_now=False)
+    pagina_inicio = models.PositiveIntegerField()
+    pagina_fin = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.libro
+    class Meta:
+        ordering = ['libro']
+        get_latest_by = ['fecha', 'libro']
+        verbose_name = 'Próologo de libro'
+        verbose_name_plural = 'Próologos de libros'
+
+
+class ResenaLibro(models.Model):
+    libro = models.ForeignKey(Libro)
+    autores = models.ManyToManyField(User, related_name='resena_libro_autores')
+    agradecimientos = models.ManyToManyField(User, related_name='resena_libro_agradecimientos')
+    fecha = models.DateField(auto_now=False)
+    pagina_inicio = models.PositiveIntegerField()
+    pagina_fin = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.libro
+    class Meta:
+        ordering = ['libro']
+        get_latest_by = ['fecha', 'libro']
+        verbose_name = 'Reseña de libro'
+        verbose_name_plural = 'Reseñas de libros'
 
 
 class DesarrolloTecnologico(models.Model):
     nombre_desarrollo_tecnologico = models.CharField(max_length=255, unique=True)
     tipo_desarrollo_tecnologico = models.ForeignKey(TipoDesarrollo)
+    proyectos = models.ManyToManyField(Proyecto, related_name='desarrollo_tecnologico_proyectos')
     descripcion = models.TextField()
     version = models.CharField(max_length=100)
-    patente = models.ForeignKey(Patente)
+    patente = models.CharField(max_length=255, blank=True)
     licencia = models.ForeignKey(Licencia)
     url = models.URLField(blank=True)
     autores = models.ManyToManyField(User, related_name='desarrollo_tecnologico_autores')
@@ -97,5 +170,3 @@ class DesarrolloTecnologico(models.Model):
         ordering = ['nombre_desarrollo_tecnologico']
         get_latest_by = ['fecha', 'nombre_desarrollo_tecnologico']
         verbose_name_plural = 'Desarrollos Tecnológicos'
-
-
