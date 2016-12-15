b5ca51b05cc7d7c5c10ff84a665de66ffbc92648
NizhiSaeba <matehereall@gmail.com>
2016-12-14 22:56:03 -0600

    ----

--- desarrollo_tecnologico/admin.py
+++ desarrollo_tecnologico/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import TipoDesarrollo, Patente, Licencia, DesarrolloTecnologico
+from . models import TipoDesarrollo, Patente, Licencia, Libro, Editorial, StatusCapituloLibro, CapituloLibro, DesarrolloTecnologico
 
+admin.site.register(TipoDesarrollo)
 admin.site.register(Patente)
 admin.site.register(Licencia)
+admin.site.register(Libro)
+#admin.site.register(Editorial)
+admin.site.register(StatusCapituloLibro)
+admin.site.register(CapituloLibro)
 admin.site.register(DesarrolloTecnologico)
--- desarrollo_tecnologico/models.py
+++ desarrollo_tecnologico/models.py
@@ -8,7 +8,7 @@ from nucleo.models import Tag, Pais, Estado, Ciudad, Ubicacion, Institucion, Dep
 
 class TipoDesarrollo(models.Model):
     tipo_desarrollo = models.CharField(max_length=255, unique=True)
-    slug = AutoSlugField(populate_from=tipo_desarrollo, unique=True)
+    slug = AutoSlugField(populate_from='tipo_desarrollo', unique=True)
 
     def __str__(self):
         return self.tipo_desarrollo
@@ -16,7 +16,7 @@ class TipoDesarrollo(models.Model):
 
 class Patente(models.Model):
     patente = models.CharField(max_length=255, unique=True)
-    slug = AutoSlugField(populate_from=patente, unique=True)
+    slug = AutoSlugField(populate_from='patente', unique=True)
 
     def __str__(self):
         return self.patente
@@ -24,10 +24,57 @@ class Patente(models.Model):
 
 class Licencia(models.Model):
     licencia = models.CharField(max_length=255, unique=True)
-    slug = AutoSlugField(populate_from=licencia, unique=True)
+    slug = AutoSlugField(populate_from='licencia', unique=True)
 
     def __str__(self):
         return self.licencia
+
+
+class Libro(models.Model):
+    libro = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='libro', unique=True)
+
+    def __str__(self):
+        return self.libro
+
+
+class Editorial(models.Model):
+    editorial = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='editorial', unique=True)
+
+    def __str__(self):
+        return self.editorial
+
+
+class StatusCapituloLibro(models.Model):
+    status = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='status', unique=True)
+
+    def __str__(self):
+        return self.status
+
+
+class CapituloLibro(models.Model):
+    capitulo = models.CharField(max_length=255)
+    libro = models.ForeignKey(Libro)
+    autores = models.ManyToManyField(User, related_name='capitulo_libro_autores')
+    editorial = models.ForeignKey(Editorial)
+    pais = models.ForeignKey(Pais)
+    fecha = models.DateField(auto_now=False)
+    pagina_inicio = models.PositiveIntegerField()
+    paginas_fin = models.PositiveIntegerField()
+    agradecimientos = models.ManyToManyField(User, related_name='capitulo_libro_agradecimientos')
+    tags = models.ManyToManyField(Tag, related_name='capitulo_libro_tags')
+    isbn = models.SlugField(max_length=17)
+    status = models.ForeignKey(StatusCapituloLibro) # este campo muy seguramente debería cambiarse a un choices o a una clase
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return "{} : {} : {} : {}".format(self.libro, self.capitulo, self.isbn, self.pais)
+    class Meta:
+        ordering = ['libro', 'capitulo']
+        get_latest_by = ['fecha', 'libro', 'capitulo']
+        verbose_name_plural = 'Capítulos de Libros'
 
 
 class DesarrolloTecnologico(models.Model):
@@ -38,10 +85,17 @@ class DesarrolloTecnologico(models.Model):
     patente = models.ForeignKey(Patente)
     licencia = models.ForeignKey(Licencia)
     url = models.URLField(blank=True)
-    autores = models.ManyToManyField(User, related_name='autor')
-    agradecimientos = models.ManyToManyField(User, related_name='agradecimiento')
-    tags = models.ManyToManyField(Tag, related_name='nombre_desarrollo_tecnologico')
+    autores = models.ManyToManyField(User, related_name='desarrollo_tecnologico_autores')
+    agradecimientos = models.ManyToManyField(User, related_name='desarrollo_tecnologico_agradecimientos')
+    tags = models.ManyToManyField(Tag, related_name='desarrollo_tecnologico_tags')
     fecha = models.DateField()
     slug = AutoSlugField(populate_from=nombre_desarrollo_tecnologico, unique=True)
+
+    def __str__(self):
+        return self.nombre_desarrollo_tecnologico
+    class Meta:
+        ordering = ['nombre_desarrollo_tecnologico']
+        get_latest_by = ['fecha', 'nombre_desarrollo_tecnologico']
+        verbose_name_plural = 'Desarrollos Tecnológicos'
 
 
