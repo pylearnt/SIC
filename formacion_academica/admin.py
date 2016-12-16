44a8e23e16ca2c27f322f72753d27419ad6ee790
NizhiSaeba <matehereall@gmail.com>
2016-12-15 23:15:01 -0600

    15-12-2016.2

--- formacion_academica/admin.py
+++ formacion_academica/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import AreaEspecializacion, TipoCurso, CursoEspecializacion, Carrera, Licenciatura, AreaConocimiento, \
+from . models import TipoCurso, CursoEspecializacion, Carrera, Licenciatura, AreaConocimiento, \
     Maestria
 
-admin.site.register(AreaEspecializacion)
+admin.site.register(AreaConocimiento)
 admin.site.register(TipoCurso)
 admin.site.register(CursoEspecializacion)
 admin.site.register(Carrera)
 admin.site.register(Licenciatura)
-admin.site.register(AreaConocimiento)
 admin.site.register(Maestria)
--- formacion_academica/models.py
+++ formacion_academica/models.py
@@ -6,17 +6,17 @@ from nucleo.models import Tag, Pais, Ubicacion, Institucion, Dependencia
 # Create your models here.
 
 
-class AreaEspecializacion(models.Model):
-    area_especializacion = models.CharField(verbose_name='Área de conocimiento', max_length=255, unique=True)
-    descripcion = models.TextField(verbose_name='Descripición', blank=True)
-    slug = AutoSlugField(populate_from='area_especializacion', unique=True)
+class AreaConocimiento(models.Model):
+    area_conocimiento = models.CharField('Área de conocimiento', max_length=255, unique=True)
+    descripcion = models.TextField(blank=True)
+    slug = AutoSlugField(populate_from='area_conocimiento', unique=True)
 
     def __str__(self):
-        return self.area_especializacion
+        return self.area_conocimiento
     class Meta:
-        ordering = ['area_especializacion']
-        verbose_name = 'Área de especialización'
-        verbose_name_plural = 'Áreas de especialización'
+        ordering = ['area_conocimiento']
+        verbose_name = 'Área de conocimiento'
+        verbose_name_plural = 'Áreas de conocimiento'
 
 
 class TipoCurso(models.Model):
@@ -38,7 +38,7 @@ class CursoEspecializacion(models.Model):
     tipo = models.ForeignKey(TipoCurso)
     inicio = models.DateField('Fecha de inicio', auto_now=False)
     fin = models.DateField('Fecha de finalización', blank=True)
-    area_especializacion = models.ForeignKey(AreaEspecializacion, verbose_name='Área de especialización')
+    area_especializacion = models.ForeignKey(AreaConocimiento, verbose_name='Área de especialización')
     instituciones = models.ManyToManyField(Dependencia, related_name='cursos_especializacion')
     ubicacion = models.ForeignKey(Ubicacion, verbose_name='Ubicación')
     tags = models.ManyToManyField(Tag, related_name='cursos_especializacion', blank=True)
@@ -83,17 +83,7 @@ class Licenciatura(models.Model):
         ordering = ['fecha_grado', 'carrera']
 
 
-class AreaConocimiento(models.Model):
-    area_conocimiento = models.CharField('Área de conocimiento', max_length=255, unique=True)
-    descripcion = models.TextField(blank=True)
-    slug = AutoSlugField(populate_from='area_conocimiento', unique=True)
 
-    def __str__(self):
-        return self.area_especializacion
-    class Meta:
-        ordering = ['area_conocimiento']
-        verbose_name = 'Área de conocimiento'
-        verbose_name_plural = 'Áreas de conocimiento'
 
 
 class Maestria(models.Model):
